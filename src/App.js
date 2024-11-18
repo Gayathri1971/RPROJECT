98bb5703fc9cdc915a9b00980ca7d465a0ac4db8
Gayathri1971 <pgayathri567@gmail.com>
2024-11-18 14:07:26 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./Components/Navbar";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+     
+    </div>
+  );
+}
+
+export default App;
